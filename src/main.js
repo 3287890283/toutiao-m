9e4ab96e11e369051b4d34bcc95ae5a80a79f786
woniu <103372189+3287890283@users.noreply.github.com>
2023-03-14 10:04:24 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/styles/index.css'
+// 加载全局样式
+import '@/styles/index.less'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
